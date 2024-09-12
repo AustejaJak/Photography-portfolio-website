110aa57e7f897170adc412eda6f3ff40332fc02f
AustejaJakimaviciute <austeja.jakimaviciute@ktu.edu>
2024-09-12 14:14:37 +0300

    Fixed marker animation responsivness

--- js/app.js
+++ js/app.js
@@ -44,87 +44,98 @@ jQuery(function () {
   var widthGain = 1; // 1 default
   var heightGain = 1; // 1 default
 
-  // repeat for all markers
-  markers.each(function () {
-    // Define variables
-    var marker = $(this),
-      width = marker.width(),
-      height = 2 * marker.height(),
-      ns = "http://www.w3.org/2000/svg";
+  function updateMarkers() {
+    markers.each(function () {
+      // Define variables
+      var marker = $(this),
+        width = marker.width(),
+        height = 2 * marker.height(),
+        ns = "http://www.w3.org/2000/svg";
 
-    var svg = document.createElementNS(ns, "svg");
+      // Remove existing SVG if any, to avoid appending multiple SVG elements on resize
+      marker.find("svg").remove();
 
-    $(svg)
-      .css({
-        width: width,
-        height: height,
-        transform:
-          "scale(" + (2 * widthGain * width) / height + "," + heightGain + ")"
-      })
-      .attr({
-        width: width,
-        height: height,
-        viewBox: "-1 -1 2 2"
-      });
+      var svg = document.createElementNS(ns, "svg");
 
-    marker[0].appendChild(svg);
+      $(svg)
+        .css({
+          width: width,
+          height: height,
+          transform:
+            "scale(" + (2 * widthGain * width) / height + "," + heightGain + ")",
+        })
+        .attr({
+          width: width,
+          height: height,
+          viewBox: "-1 -1 2 2",
+        });
 
-    var path = document.createElementNS(ns, "path");
+      marker[0].appendChild(svg);
 
-    $(path)
-      .attr({
-        pathLength: 100,
-        "vector-effect": "non-scaling-stroke"
-      })
-    svg.appendChild(path);
-
-    setCircle(false);
-    setCircle(false);
-
-    marker.mouseover(function () {
-      setCircle(true);
-    });
-
-    function setCircle(show_element) {
-      if (show_element) {
-        $(path).css("visibility", "visible");
-      } else {
-        $(path).css("visibility", "hidden");
-      }
-
-      var pathLength = 1000 * path.getTotalLength();
+      var path = document.createElementNS(ns, "path");
 
       $(path)
         .attr({
-          d: circlePath(-0.15,0.05,150,190,0.05,0.3)
-        })
-        .attr({
-          "stroke-dasharray": pathLength,
-          "stroke-dashoffset": pathLength
+          pathLength: 100,
+          "vector-effect": "non-scaling-stroke",
         });
-    }
+      svg.appendChild(path);
 
-    function circlePath(dr_min, dr_max, θ0_min, θ0_max, dθ_min, dθ_max) {
-      var c = 0.551915024494,
-        β = Math.atan(c),
-        d = Math.sqrt(c * c + 1 * 1),
-        r = 0.9,
-        θ = ((θ0_min + Math.random() * (θ0_max - θ0_min)) * Math.PI) / 180,
-        path = "M";
+      setCircle(false);
 
-      path += [r * Math.sin(θ), r * Math.cos(θ)];
-      path += " C" + [d * r * Math.sin(θ + β), d * r * Math.cos(θ + β)];
+      marker.mouseover(function () {
+        setCircle(true);
+      });
 
-      for (var i = 0; i < 4; i++) {
-        θ += (Math.PI / 2) * (1 + dθ_min + Math.random() * (dθ_max - dθ_min));
-        r *= 1 + dr_min + Math.random() * (dr_max - dr_min);
-        path +=
-          " " +
-          (i ? "S" : "") +
-          [d * r * Math.sin(θ - β), d * r * Math.cos(θ - β)];
-        path += " " + [r * Math.sin(θ), r * Math.cos(θ)];
+      function setCircle(show_element) {
+        if (show_element) {
+          $(path).css("visibility", "visible");
+        } else {
+          $(path).css("visibility", "hidden");
+        }
+
+        var pathLength = 1000 * path.getTotalLength();
+
+        $(path)
+          .attr({
+            d: circlePath(-0.15, 0.05, 150, 190, 0.05, 0.3),
+          })
+          .attr({
+            "stroke-dasharray": pathLength,
+            "stroke-dashoffset": pathLength,
+          });
       }
-      return path;
-    }
+
+      function circlePath(dr_min, dr_max, θ0_min, θ0_max, dθ_min, dθ_max) {
+        var c = 0.551915024494,
+          β = Math.atan(c),
+          d = Math.sqrt(c * c + 1 * 1),
+          r = 0.9,
+          θ = ((θ0_min + Math.random() * (θ0_max - θ0_min)) * Math.PI) / 180,
+          path = "M";
+
+        path += [r * Math.sin(θ), r * Math.cos(θ)];
+        path += " C" + [d * r * Math.sin(θ + β), d * r * Math.cos(θ + β)];
+
+        for (var i = 0; i < 4; i++) {
+          θ += (Math.PI / 2) * (1 + dθ_min + Math.random() * (dθ_max - dθ_min));
+          r *= 1 + dr_min + Math.random() * (dr_max - dr_min);
+          path +=
+            " " +
+            (i ? "S" : "") +
+            [d * r * Math.sin(θ - β), d * r * Math.cos(θ - β)];
+          path += " " + [r * Math.sin(θ), r * Math.cos(θ)];
+        }
+        return path;
+      }
+    });
+  }
+
+  // Initial call to update markers
+  updateMarkers();
+
+  // Recalculate on window resize
+  $(window).resize(function () {
+    updateMarkers();
   });
 });
